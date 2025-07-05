314c90bd5d705d21fa71c181d7c22b2d9e7d69f6
Yoga Pratama <yogapr.dev@gmail.com>
2025-07-05 11:27:48 +0700

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { TodoComponent } from './todo/todo.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'todo',
+    component: TodoComponent,
+  },
+];
